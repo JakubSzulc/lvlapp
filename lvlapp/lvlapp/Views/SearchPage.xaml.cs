b11b27de58e930286b3db6f6b556e01f32299ef0
JakubSzulc <SzulcJakub@protonmail.com>
2022-04-12 20:41:58 +0200

    Added category in Search Page and change name this page.

---
+++ lvlapp/lvlapp/Views/SearchPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Windows.Input;
+using Xamarin.Essentials;
+
+namespace lvlapp.Views
+{
+    public partial class SearchPage : ContentPage 
+    {
+        public SearchPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void OnSearchBarButtonPressed(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void Button_Biznes_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Finanse_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Gastronomia_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Uroda_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Budownictwo_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Sztuka_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Sport_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Zdrowie_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
